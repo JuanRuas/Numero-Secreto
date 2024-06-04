f626e588961cfd3fb945029d74f99e173fe76c94
JuanRuas <juan_ruas@outlook.com>
2024-06-04 15:56:06 -0300

    alterando limite do jogo para 100

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,4 +1,4 @@
-let numeroLimite = 10;
+let numeroLimite = 100;
 let listaDeNumerosSorteados = [];
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
  }
     mensagemInicial();
 
